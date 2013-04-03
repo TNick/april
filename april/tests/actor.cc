70ef930e4f3447a2e78faf60626d1c1d50e9f48c
Nicu Tofan <nicu.tofan@gmail.com>
2013-04-03 11:30:24 +0300

    Added actor tests

---
+++ april/tests/actor.cc
@@ -0,0 +1,77 @@
+#include	<gtest/gtest.h>
+#include	<april/aprillibrary.h>
+#include	<april/logic/world.h>
+#include	<april/logic/actor.h>
+#include	<april/logic/actorfactory.h>
+
+using namespace april;
+
+TEST(Agent, init) {
+
+	initAprilLibrary();
+	World * w = new World( "test-world", 1000 );
+	DEC_REF( w, w );
+
+	Actor * ag = new Actor( w );
+	DEC_REF(ag,ag);
+	EXPECT_TRUE( ag->firstSensor() == NULL );
+	EXPECT_TRUE( ag->firstActuator() == NULL );
+	EXPECT_TRUE( ag->firstReflex() == NULL );
+	EXPECT_TRUE( ag->firstBrain() == NULL );
+	EXPECT_EQ( ag->kind(), InvalidId );
+	EXPECT_EQ( ag->kindName(), QString() );
+	EXPECT_EQ( ag->birth(), (quint64)0 );
+	EXPECT_EQ( ag->death(), (quint64)0 );
+	EXPECT_EQ( ag->age(), (quint64)0 );
+	EXPECT_EQ( ag->toLive(), (quint64)0 );
+	EXPECT_EQ( ag->energy(), (quint64)0 );
+	EXPECT_EQ( ag->cost(), (quint64)0 );
+	EXPECT_EQ( ag->isAlive(), false );
+	EXPECT_EQ( ag->dies(), true );
+	EXPECT_TRUE( ag->payload().p_ == NULL );
+	
+	endAprilLibrary();
+}
+
+enum TestId	{
+	TestId = 1
+};
+
+class TstFact : public ActorFactory	{
+public:
+	
+	TstFact( World * w ) 
+		: ActorFactory( w )
+	{
+		addMyself( TestId );
+	}
+	
+	virtual Actor *			create				( ID id )
+	{
+		Actor * ret = new Actor( world() );
+		
+		return ret;
+	}
+
+	virtual void			copyDefaultDNA		( DNA & destination )
+	{ 
+		destination = defaultDNA();
+	}
+
+};
+
+TEST(Agent, factory) {
+	
+	initAprilLibrary();
+	World * w = new World( "test-world", 1000 );
+	DEC_REF( w, w );
+	
+	
+
+	TstFact * fact = new TstFact( w );
+	DEC_REF( w, w );
+	
+	
+	
+	endAprilLibrary();
+}
